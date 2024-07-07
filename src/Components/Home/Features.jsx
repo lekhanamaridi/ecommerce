7e32016cff65ad1a02ce678b0859c29fd83220b2
Amit-Bongale <amitbongale14@gmail.com>
2024-07-08 00:16:32 +0530

    features style corrected

--- src/Components/Home/Features.jsx
+++ src/Components/Home/Features.jsx
@@ -5,37 +5,37 @@ import returns from '../../Assets/Featuresimages/returns.svg'
 
 function Features() {
     return(
-        <div class="flex flex-nowrap pl-60">
+        <div class="flex flex-wrap items-center justify-around my-4">
 
-            <div class="w-[320px] h-[100px] bg-green-100 rounded-lg border-2 border-green-300">
+            <div class="w-[320px] h-[100px] bg-green-100 rounded-lg border-2 border-green-500">
                 <img src={pricetag} class="float-left scale-100 pl-5 pt-3"></img>
                 <div class="pt-2 pl-32">
                     <p class="text-xl text-black"><b><i>Best prices & offers</i></b></p> 
-                    <p class="text-base italic text-gray-500">Orders $50 or more</p>
+                    <p class="text-base  text-gray-500">Orders $50 or more</p>
                 </div>
             </div>
 
-            <div class="w-[320px] h-[100px] bg-green-100 rounded-lg border-2 border-green-300">
+            <div class="w-[320px] h-[100px] bg-green-100 rounded-lg border-2 border-green-500">
                 <img src={freedelivery} class="float-left scale-100 pl-5 pt-3"></img>
-                <div class="pt-2 pl-32">
+                <div class="pt-5 pl-32">
                     <p class="text-xl text-black"><b><i>Free delivery</i></b></p> 
-                    <p class="text-base italic text-gray-500">24/7 amazing services</p>
+                    <p class="text-base  text-gray-500">24/7 amazing services</p>
                 </div>
             </div>
             
-            <div class="w-[320px] h-[100px] bg-green-100 rounded-lg border-2 border-green-300">
+            <div class="w-[320px] h-[100px] bg-green-100 rounded-lg border-2 border-green-500">
                 <img src={deals} class="float-left scale-100 pl-5 pt-3"></img>
-                <div class="pt-2 pl-32">
+                <div class="pt-5 pl-32">
                     <p class="text-xl text-black"><b><i>Great daily deal</i></b></p>
-                    <p class="text-base italic text-gray-500">When you sign up</p>
+                    <p class="text-base text-gray-500">When you sign up</p>
                 </div>
             </div>
             
-            <div class="w-[320px] h-[100px] bg-green-100 rounded-lg border-2 border-green-300 ">
+            <div class="w-[320px] h-[100px] bg-green-100 rounded-lg border-2 border-green-500 ">
                 <img src={returns} class="float-left scale-100 pl-5 pt-3"></img>
-                <div class="pt-2 pl-32">
+                <div class="pt-5 pl-32">
                     <p class="text-xl text-black"><b><i>Easy returns</i></b></p> 
-                    <p class="text-base italic text-gray-500">Within 30 days</p>
+                    <p class="text-base  text-gray-500">Within 30 days</p>
                 </div>
             </div>
         </div>
