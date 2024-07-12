0cc3a1f3d664b4fca5957c84e95544e9a5db1b85
lekhanamaridi <lekhanamaridi444@gmail>
2024-07-12 15:18:50 +0530

    a

--- src/Components/CustomRouter.js
+++ src/Components/CustomRouter.js
@@ -7,6 +7,7 @@ import Login from "./Login/Login";
 import Signup from "./Login/Signup";
 import Contactus from "./Contactus/Contactus";
 import Categories from "./Category/Categories";
+import Checkout from "./Checkout/Checkout";
 
 function CustomRouter() {
   return (
@@ -20,6 +21,7 @@ function CustomRouter() {
       <Route path="/signup" element={<Signup />} />
       <Route path="/contactus" element={<Contactus />} />
       <Route path="/categories" element={<Categories />} />
+      <Route path="/checkout" element={<Checkout />} />
 
     </Routes>
   )
