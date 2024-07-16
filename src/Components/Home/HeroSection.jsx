8d955b604f1892ea44f91cfd5d4f6c15b8848440
Amit-Bongale <amitbongale14@gmail.com>
2024-07-16 21:49:02 +0530

    links added

--- src/Components/Home/HeroSection.jsx
+++ src/Components/Home/HeroSection.jsx
@@ -13,7 +13,7 @@ function HeroSection() {
             <img src={mobile} alt="mobile" className='relative bg-clip-content left-[500px] top-[-50px]' />
 
             <button class="relative top-[-300px] w-[150px] font-medium bg-white h-[50px] flex items-center justify-center rounded-md cursor-pointer overflow-hidden transition-all duration-300 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-300 before:ease-in-out before:z-[-1] before:rounded-sm  hover:before:left-0 hover:text-white">
-                <Link to={'/products/mobiles'}> Shop Now </Link>
+                <Link to={'/details/mobiles/11'}> Shop Now </Link>
             </button>
             
         </div>
